cf8afdd827b1c857805eca19574479059edd44b2
ab-h-i-n <abhinshelly@gmail.com>
2024-09-05 20:04:30 +0530

    fix : build error

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getDatabase, ref, set } from "firebase/database";
 // Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API,
-  authDomain: NEXT_PUBLIC_FIREBASE_DOMAIN,
-  databaseURL: NEXT_PUBLIC_FIREBASE_DB,
-  projectId: NEXT_PUBLIC_PROJ_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_MSG_SENDER_ID,
-  appId: NEXT_PUBLIC_APP_ID,
-  measurementId: NEXT_PUBLIC_MEASUREMENT_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB,
+  projectId: process.env.NEXT_PUBLIC_PROJ_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MSG_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
